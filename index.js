1eb861b95bc48aeb9c50498471ce536b5b6bf6e7
herdeybayor <shereefdeen4real@gmail.com>
2022-11-05 23:30:45 +0100

    feat: create header 🎉

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+
+console.log(hamburger);
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("is-active");
+//     console.log("clicked");
+// });
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primaryCyan: "hsl(180, 66%, 49%)",
+                primaryViolet: "hsl(257, 27%, 26%)",
+                secondaryRed: "hsl(0, 87%, 67%)",
+                neutralGray: "hsl(0, 0%, 75%)",
+                neutralGrayishViolet: "hsl(257, 7%, 63%)",
+                neutralDarkBlue: "hsl(255, 11%, 22%)",
+                neutralDarkViolet: "hsl(260, 8%, 14%)",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
+    },
+};
